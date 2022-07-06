22536811452f6b64943915f2bc2776541ae0eecc
roottool <operation.versatile.roottool@gmail.com>
2022-07-06 21:05:10 +0900

    Add some devdependencies (#5)
    chore: Add some devdependencies

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+module.exports = config;
